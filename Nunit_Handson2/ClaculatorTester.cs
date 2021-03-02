00190e174bf91f7206f98cdcfa497a2c55aedfa9
ramyavisalatchi <38558679+ramyavisalatchi@users.noreply.github.com>
2021-03-02 15:25:54 +0530

    Add files via upload

---
+++ Nunit_Handson2/ClaculatorTester.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CalcLibrary;
+namespace Calc_Tester
+{
+    public class ClaculatorTester
+    {
+        int Result;
+        [SetUp]
+        public void SetUpMethod()
+        {
+            Result = 0;
+        }
+
+        [Test]
+        [TestCase(20, 5, 15)]
+        [TestCase(20, 4, 16)]
+        public void CalcSubMethod(int x, int y, int expected)
+        {
+            SimpleCalculator add = new SimpleCalculator();
+            Result = add.Subtraction(x, y);
+            Assert.AreEqual(expected, Result);
+        }
+
+        [Test]
+        [TestCase(3, 5, 15)]
+        [TestCase(2, 4, 8)]
+        public void CalcMulMethod(int x, int y, int expected)
+        {
+            SimpleCalculator add = new SimpleCalculator();
+            Result = add.Multiplication(x, y);
+            Assert.AreEqual(expected, Result);
+        }
+
+        [Test]
+        [TestCase(20, 2, 10)]
+        [TestCase(20, 0, 0)]
+        public void CalcDivMethod(int x, int y, int expected)
+        {
+            SimpleCalculator add = new SimpleCalculator();
+            try
+            {
+                    Result = add.Division(x, y);
+                    Assert.AreEqual(expected, Result);
+            }
+            catch (ArgumentException e)
+            {
+                Assert.Fail("Division by zero");
+            }
+        }
+
+        [Test]
+        [TestCase(2, 5, 7)]
+        public void TestAddAndClear(int x,int y,int expected)
+        {
+            SimpleCalculator add = new SimpleCalculator();
+            add.Addition(x, y);
+            Assert.AreEqual(add.GetResult, expected);
+            add.AllClear();
+            Assert.AreEqual(add.GetResult, 0);
+        }
+
+
+
+    }
+}
---
+++ Nunit_Handson2/MathLibrary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CalcLibrary
+{
+    interface IMathLibrary
+    {
+        int Addition(int a, int b);
+        int Subtraction(int a, int b);
+        int Multiplication(int a, int b);
+        int Division(int a, int b);
+    }
+
+    public class SimpleCalculator : IMathLibrary
+    {
+        int result = 0;
+        public int Addition(int a, int b)
+        {
+            result = a + b;
+            return result;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            result = a - b;
+            return result;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            result = a * b;
+            return result;
+        }
+
+        public int Division(int a, int b)
+        {
+            if (b == 0)
+                throw new ArgumentException("Second Parameter Can't be Zero");
+            result = a / b;
+            return result;
+        }
+
+        public void AllClear()
+        {
+            result = 0;
+        }
+
+        public int GetResult
+        {
+            get { return result; }            
+        }
+    }
+}
