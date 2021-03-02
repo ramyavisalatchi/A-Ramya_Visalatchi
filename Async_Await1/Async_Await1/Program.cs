a3d0e05bd93d86504ec46dc35fe5c87b73d9c3e5
ramyavisalatchi <38558679+ramyavisalatchi@users.noreply.github.com>
2021-03-02 15:18:26 +0530

    Add files via upload

---
+++ Async_Await1/Async_Await1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async_Await1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Method1();
+            Console.Read();
+        }
+
+        public static async Task Method1()
+        {
+            Console.WriteLine("Method 1 called");
+            var a= await Method2();
+            Console.WriteLine("Method 2 returned value");
+            Console.WriteLine(a);
+            
+        }
+
+        public static async Task<string> Method2()
+        {
+            string b = "hello";
+            return b;
+        }
+
+    }
+}
---
+++ Multithreading Sample/Multithreading Sample/Printer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Multithreading_Sample
+{
+    class Printer
+    {
+        private object lockToken = new object();
+        public void PrintNumbers()
+        {
+            lock (lockToken)
+            {
+                //Display Thread info
+                Console.WriteLine("Thread-> {0} started @{1} and executing PrintNumbers() method", Thread.CurrentThread.ManagedThreadId, DateTime.Now.ToLongTimeString());
+
+
+                Console.Write("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("{0}, ", i);
+                    Thread.Sleep(1000);
+                }
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
