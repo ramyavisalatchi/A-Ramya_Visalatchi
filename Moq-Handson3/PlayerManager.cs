0954c1367ea0497253c784f24e976dc057df5dc9
ramyavisalatchi <38558679+ramyavisalatchi@users.noreply.github.com>
2021-03-02 15:34:59 +0530

    Add files via upload

---
+++ Moq-Handson3/PlayerManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace Moq_Handson3
+{
+    public interface IPlayerMapper
+
+    {
+
+        bool IsPlayerNameExistsInDb(string name);
+
+        void AddNewPlayerIntoDb(string name);
+
+    }
+    public class PlayerManager:IPlayerMapper
+    {
+
+        private readonly string _connectionString =
+
+"Data Source=(local);Initial Catalog=GameDB;Integrated Security=True";
+
+        public bool IsPlayerNameExistsInDb(string name)
+
+        {
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+
+            {
+
+                connection.Open();
+
+                using (SqlCommand command = connection.CreateCommand())
+
+                {
+
+                    command.CommandText = "SELECT count(*) FROM Player WHERE 'Name' = @name";
+
+                    command.Parameters.AddWithValue("@name", name);
+
+                    // Get the number of player with this name
+
+                    var existingPlayersCount = (int)command.ExecuteScalar();
+
+                    // Result is 0, if no player exists, or 1, if a player already exists
+
+                    return existingPlayersCount > 0;
+
+                }
+
+            }
+
+        }
+
+        public void AddNewPlayerIntoDb(string name)
+
+        {
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+
+            {
+
+                connection.Open();
+
+                using (SqlCommand command = connection.CreateCommand())
+
+                {
+
+                    command.CommandText = "INSERT INTO Player ([Name]) VALUES (@name)";
+
+                    command.Parameters.AddWithValue("@name", name);
+
+                    command.ExecuteNonQuery();
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Moq-Handson3/PlayerManagerTester.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq_Handson3;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlayerManager.Tests
+    {
+        [TestFixture]
+        public class PlayerManagerTests
+        {
+            Mock<IPlayerMapper> _mock;
+
+            [OneTimeSetUp]
+            public void Init()
+            {
+                _mock = new Mock<IPlayerMapper>(MockBehavior.Loose);
+            }
+
+            [Test]
+            [TestCase("John")]
+
+            [TestCase("Bobby")]
+
+            public void RegisterNewPlayer_whencalled_RegisterANewPlayer(string name)
+            {
+                _mock.Setup(p => p.IsPlayerNameExistsInDb(name)).Returns(false);
+                Player result = Player.RegisterNewPlayer(name, _mock.Object);
+                NUnit.Framework.Assert.AreEqual(result.Name, name);
+
+            }
+            [Test]
+            [TestCase(null)]
+            [TestCase("")]
+            [TestCase("a")]
+            [ExpectedException(typeof(ArgumentException))]
+            public void RegisterNewPlayer_whencalled_ReturnsArgumentException(string name)
+            {
+                Player result = Player.RegisterNewPlayer(name, _mock.Object);
+            }
+
+        }
+
+    }
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using NUnit.Framework;
+//using Moq;
+//using Moq_Handson3;
+//namespace PlayerManager_Tests
+//{
+//    [TestFixture]
+//    public class PlayerManagerTester
+//    {
+//        [Test]
+//        public void OneTimeSetUp(string name,  bool expected, IPlayerMapper playerMapper = null)
+//        {
+//            Mock<IPlayerMapper> mock = new Mock<IPlayerMapper>();
+
+
+//            mock.Setup(p => p.IsPlayerNameExistsInDb(name)).Returns(expected);
+
+//            var obj = new Player();
+
+//            bool actual = obj.RegisterNewPlayer(name, mock.Object);
+//            Assert.AreEqual(expected, actual);
+//        }
+//    }
+//}
