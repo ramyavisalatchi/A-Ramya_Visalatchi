cfd370475414dacae8aaa0e4214a5b35a58a93aa
ramyavisalatchi <38558679+ramyavisalatchi@users.noreply.github.com>
2021-03-04 20:59:41 +0530

    Add files via upload

---
+++ ASP/AspCore4/AspCore44/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCore44.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public bool IsPermanent { get; set; }
+    }
+}
