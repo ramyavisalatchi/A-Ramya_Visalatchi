b2298401c94f804a4876044c7ab45650be66c620
ramyavisalatchi <ramyavisalatchi@gmail.com>
2021-03-06 12:24:45 +0530

    1

---
+++ Moq_Handson1/CustomerCommLib/CustomeComm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCommLib
+{
+    class CustomeComm
+    {
+        IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+
+        {
+
+            _mailSender = mailSender;
+
+        }
+
+        public bool SendMailToCustomer()
+
+        {
+
+            //Actual logic goes here
+
+            //define message and mail address
+
+            _mailSender.SendMail(cust123@abc.com,”Some Message”);
+
+            return true;
+        }
+    }
+}
---
+++ Moq_Handson2/Moq_Handson2/DirectoryExplorer_Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using Moq_Handson2;
+using static Moq_Handson2.DirectoryExplore;
+
+namespace Moq_Handson2
+{
+    class DirectoryExplorer_Tests
+    {
+        private readonly string _file1 = "file.txt";
+
+        private readonly string _file2 = "file2.txt";
+        Mock<IDirectoryExplorer> mock;
+        [OneTimeSetUp]
+        public void init()
+        {
+            mock = new Mock<IDirectoryExplorer>();
+        }
+
+        [Test]
+        [TestCase("filepath")]
+
+        public void TestMethod(string path)
+        {
+            ICollection<string> hardcode = new List<string>() { _file1, _file2 };
+            mock.Setup(p => p.GetFiles(path)).Returns(hardcode);
+
+            Assert.IsNotNull(mock.Object.GetFiles(path));
+            Assert.AreEqual(2, mock.Object.GetFiles(path).Count);
+
+            Assert.Contains(_file1, (System.Collections.ICollection)mock.Object.GetFiles(path));
+
+        }
+        }
+    }
