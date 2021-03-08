728f3e18dc5888a95c1e5f412eb4f001fad03e11
ramyavisalatchi <ramyavisalatchi@gmail.com>
2021-03-08 15:33:49 +0530

    Updated

---
+++ ASP-Core/AspCore6/AspCore/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using AspCore.Models;
+using ASPCoreLog4Net.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCoreLog4Net.Controllers
+{
+    [HandleException]
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+        private readonly ILoggerManager _logger;
+
+        public HomeController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            throw new Exception("Error happened in index");
+            _logger.LogInformation("Running Home/Index");
+            return View();
+        }
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP-Core/AspCore6/AspCore/Program.cs
@@ -0,0 +1,32 @@
+using ASPCoreLog4Net.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCoreLog4Net
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            var logger = host.Services.GetRequiredService<ILoggerManager>();
+            logger.LogInformation("Starting host application");
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
