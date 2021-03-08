d79facf59ce9af9186193865a43e95f5f2570094
ramyavisalatchi <ramyavisalatchi@gmail.com>
2021-03-08 10:39:54 +0530

    updated

---
+++ ASP-Core/AspCore7/AspCore7/Data/AspCore7Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AspCore7.Models;
+
+namespace AspCore7.Data
+{
+    public class AspCore7Context : DbContext
+    {
+        public AspCore7Context (DbContextOptions<AspCore7Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AspCore7.Models.Account> Account { get; set; }
+    }
+}
---
+++ ASP-Core/AspCore7/AspCore7/Migrations/20210308050307_account.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspCore7.Migrations
+{
+    public partial class account : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    AccountId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.AccountId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Account");
+        }
+    }
+}
