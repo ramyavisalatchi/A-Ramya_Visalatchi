7dc7210ab6e470dfe46c1eb9919084394a6c2918
ramyavisalatchi <ramyavisalatchi@gmail.com>
2021-03-16 23:00:36 +0530

    init

--- Design Patterns/Day2_HandsOn1_Singleton/Day2_HandsOn1_Singleton/Program.cs
+++ Design Patterns/Day2_HandsOn1_Singleton/Day2_HandsOn1_Singleton/Program.cs
@@ -8,10 +8,12 @@ namespace Day2_HandsOn1_Singleton
         {
             DBConn object1 = DBConn.getInstance();
             DBConn object2 = DBConn.getInstance();
+
             if(object1.Equals(object2))
             {
                 Console.WriteLine("Objects are same");
             }
+            Console.WriteLine(object1.GetHashCode() + "  " + object2.GetHashCode());
             Console.ReadLine();
         }
     }
