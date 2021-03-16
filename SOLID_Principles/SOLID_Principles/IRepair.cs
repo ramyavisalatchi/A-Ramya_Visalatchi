4aedd53cebb1c9155fcd737084410dd6a9d77280
ramyavisalatchi <ramyavisalatchi@gmail.com>
2021-03-16 15:38:43 +0530

    init

---
+++ SOLID_Principles/SOLID_Principles/IRepair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles
+{
+    public interface IRepair
+    {
+        void ProcessPhoneRepair(string modelName);
+        void ProcessAccessoryRepair(string accessoryType);
+    }
+}
