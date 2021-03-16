5cc2b30737c2051227c57228854545d585415eab
ramyavisalatchi <ramyavisalatchi@gmail.com>
2021-03-16 17:56:58 +0530

    init

---
+++ Design Patterns/Day2_HandsOn1_Singleton/Day2_HandsOn1_Singleton/DBConn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day2_HandsOn1_Singleton
+{
+    public sealed class DBConn
+    {
+        private static DBConn _instance;
+        private DBConn()
+        {
+            Console.WriteLine("Instance is being Called");
+        }
+        public static DBConn getInstance()
+        {
+            if (_instance == null)
+                _instance = new DBConn();
+            return _instance;
+        }
+    }
+}
---
+++ Design Patterns/Day2_HandsOn1_Singleton/Day2_HandsOn1_Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day2_HandsOn1_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConn object1 = DBConn.getInstance();
+            DBConn object2 = DBConn.getInstance();
+            if(object1.Equals(object2))
+            {
+                Console.WriteLine("Objects are same");
+            }
+            Console.ReadLine();
+        }
+    }
+}
